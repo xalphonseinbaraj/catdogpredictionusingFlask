c56929d3ce8cc06c1dac75d9395d0509a66aa948
xalphonseinbaraj <47241538+xalphonseinbaraj@users.noreply.github.com>
2022-05-22 14:48:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,9 @@ def predictRoute():
     result = clApp.classifier.predictiondogcat()
     return jsonify(result)
 
-port = int(os.getenv("PORT"))
+#port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port= port)
+    #app.run(host='0.0.0.0', port= port)
+    app.run(debug=True)
     #app.run(host='0.0.0.0', port=8000, debug=True)
